e609cbbd566e297e9115c00a9d490c97225ff71d
dominic kudiabor <dominickudd@gmail.com>
2021-05-31 15:42:41 +0300

    updated logic for returning data

--- websocket.js
+++ websocket.js
@@ -1,3 +1,4 @@
+const { v4: uuidv4 } = require('uuid');
 const webSocketsServerPort = 8000;
 const webSocketServer = require('websocket').server;
 const http = require('http');
@@ -8,18 +9,10 @@ const wsServer = new webSocketServer({
   httpServer: server
 });
 
-// Generates unique ID for every new connection
-const getUniqueID = () => {
-  const s4 = () => Math.floor((1 + Math.random()) * 0x10000).toString(16).substring(1);
-  return s4() + s4() + '-' + s4();
-};
-
-// I'm maintaining all active connections in this object
+// Maintaining all active connections in this object
 const clients = {};
-// I'm maintaining all active users in this object
+// Maintaining all active users in this object
 const users = {};
-// The current editor content is maintained here.
-let editorContent = null;
 // User activity history.
 let userActivity = [];
 
@@ -28,55 +21,50 @@ let boardServerState = {}
 
 
 const sendMessage = (json) => {
-  // We are sending the current data to all connected clients
+  //Sending the current data to all connected clients
   Object.keys(clients).map((client) => {
     clients[client].sendUTF(json);
   });
 }
 
-const typesDef = {
-  USER_EVENT: "userevent",
-  CONTENT_CHANGE: "contentchange",
-  BOARD_DATA: "board-data"
-}
-
 wsServer.on('request', function(request) {
-  var userID = getUniqueID();
+  //create unique id for new user
+  var userID = uuidv4(); 
 
   console.log((new Date()) + ' Recieved a new connection from origin ' + request.origin + '.');
-  // You can rewrite this part of the code to accept only the requests from allowed origin
+
+
+//Create a new connection
   const connection = request.accept(null, request.origin);
+
+  //assign the new user with a unique id
   clients[userID] = connection;
-  console.log('connected: ' + userID + ' in ' + Object.getOwnPropertyNames(clients));
+
+  //output a connected message to verify new connection
+  console.log('connected: ' + userID );
+
+  //listen for incoming messages
   connection.on('message', function(message) {
+
      if (message.type === 'utf8') {
-        const dataFromClient = JSON.parse(message.utf8Data);
-   
-        boardServerState = dataFromClient.content
-        sendMessage(JSON.stringify(boardServerState));
-    //   const dataFromClient = JSON.parse(message.utf8Data);
-    //   const json = { type: dataFromClient.type };
-    //   if (dataFromClient.type === typesDef.USER_EVENT) {
-    //     users[userID] = dataFromClient;
-    //     userActivity.push(`${dataFromClient.username} joined to edit the document`);
-    //     json.data = { users, userActivity };
-    //   } else if (dataFromClient.type === typesDef.CONTENT_CHANGE) {
-    //     editorContent = dataFromClient.content;
-    //     json.data = { editorContent, userActivity };
-    //   }
-    //   sendMessage(JSON.stringify(json));
+
+
+       //broadcast the message to all connected clients
+for (key in clients){
+  clients[key].sendUTF(message.utf8Data)
+  console.log('Sent message to : ', key )
+}
      }
   });
 
-
-
+ 
   // user disconnected
   connection.on('close', function(connection) {
     console.log((new Date()) + " Peer " + userID + " disconnected.");
-    const json = { type: typesDef.BOARD_DATA };
+    const json = { type: 'board-data'};
     userActivity.push(`${users[userID]} left the document`);
     json.data = { users, userActivity };
-    delete clients[userID];
+    clients[userID];
     delete users[userID];
     sendMessage(JSON.stringify(json));
   });
